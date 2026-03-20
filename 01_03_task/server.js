473f71d3429a79f5fc5c0e18e9cebd915ec3bca6
bambalooon <krzysztof.adam.balon@gmail.com>
2026-03-20 19:45:27 +0100

    01_03_task RequestSchema in Zod

--- 01_03_task/server.js
+++ 01_03_task/server.js
@@ -48,8 +48,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({
-        message: "OK",
-        received: result.data,
+        msg: result.data.msg
       })
     );
   });
