fd78f7c006e8769c2fe97140b59f295f67f53160
bambalooon <krzysztof.adam.balon@gmail.com>
2026-03-25 20:42:49 +0100

    02_02_task fixing vision

--- 02_02_task/vision.js
+++ 02_02_task/vision.js
@@ -3,9 +3,13 @@
  */
 
 import log from "./src/helpers/logger.js";
-import {logStats} from "./src/helpers/stats.js";
 import {AI_DEVS_API_KEY} from "../config.js";
+import sharp from "sharp";
+import {exec} from "child_process";
+import {writeFileSync} from "fs";
 import {vision} from "./src/native/vision.js";
+
+const MIME_TYPE = "image/png";
 
 const QUERY = `
 Analyze the 3x3 board on both images.
@@ -28,15 +32,15 @@ This is empty board in ASCII format:
 This is board on 1st image in ASCII format:
 +---+---+---+
 |   |   |   |
-| +-|-+-|---|
-| | | | |   |
+| XX|XXX|XXX|
+| X | X |   |
 +---+---+---+
-| | | | |   |
-| | | +-|-+-|
-| | | | | | |
+| X | X |   |
+| X | XX|XXX|
+| X | X | X |
 +---+---+---+
-| | | | | | |
-|-+-|-+ | +-|
+| X | X | X |
+|XXX|XX | XX|
 |   |   |   |
 +---+---+---+
  
@@ -49,12 +53,38 @@ const getImageBase64 = async (image_url) => {
   return Buffer.from(buffer).toString("base64");
 }
 
+const toBlackAndWhiteBase64 = async (imageBase64) => {
+  const buffer = Buffer.from(imageBase64, "base64");
+  const bwBuffer = await sharp(buffer).grayscale().threshold(128).toBuffer();
+  return bwBuffer.toString("base64");
+};
+
+const cropImage = async (imageBase64, { left, top, width, height }) => {
+  const buffer = Buffer.from(imageBase64, "base64");
+  const sliced = await sharp(buffer)
+      .extract({ left, top, width, height })
+      .toBuffer();
+  return sliced.toString("base64");
+};
+
+const showImage = (imageBase64, filename = "preview.png") => {
+  const path = `./cropped-${filename}`;
+  writeFileSync(path, Buffer.from(imageBase64, "base64"));
+  // exec(`xdg-open ${path}`);
+};
+
 const main = async () => {
   try {
+    const solvedState = await cropImage(await getImageBase64(`https://hub.ag3nts.org/i/solved_electricity.png`), { left: 140, top: 88, width: 290, height: 290 });
+    const currentState = await cropImage(await getImageBase64(`https://hub.ag3nts.org/data/${AI_DEVS_API_KEY}/electricity.png`), { left: 236, top: 98, width: 290, height: 290 });
+    showImage(solvedState, 'solved.png');
+    showImage(currentState, 'current.png');
+    // showImage(await toBlackAndWhiteBase64(solvedOrig));
+
     const result = await vision({
       images: [
-        { imageBase64: await getImageBase64(`https://hub.ag3nts.org/i/solved_electricity.png`), mimeType: "image/png" },
-        { imageBase64: await getImageBase64(`https://hub.ag3nts.org/data/${AI_DEVS_API_KEY}/electricity.png`), mimeType: "image/png" },
+        { imageBase64: solvedState, mimeType: MIME_TYPE },
+        { imageBase64: currentState, mimeType: MIME_TYPE },
       ],
       question: QUERY
     });
