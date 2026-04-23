689bd572b9dbeec48e131137768f7bcf7db3efb5
bambalooon <krzysztof.adam.balon@gmail.com>
2026-04-23 19:03:13 +0200

    04_04_task init

---
+++ 04_04_task/verify.js
@@ -0,0 +1,18 @@
+import {AI_DEVS_API_KEY} from "../config.js";
+
+const answer = {
+    apikey: AI_DEVS_API_KEY,
+    task: "domatowo",
+    answer: {
+        action: "help"
+    }
+}
+console.log(answer);
+const response = await fetch("https://hub.ag3nts.org/verify", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(answer)
+});
+
+const data = await response.json();
+console.log(JSON.stringify(data));
